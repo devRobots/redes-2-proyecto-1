6f9c6d23bf802dcfb25028117c1ffbaf5cbd7cea
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2020-09-02 21:24:53 -0500

    Requisitos minimos: Entradas

--- src/co/edu/uniquindio/Main.java
+++ src/co/edu/uniquindio/Main.java
@@ -1,8 +1,22 @@
 package co.edu.uniquindio;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int tamanioDatagrama, mtu;
+        String respuesta = "";
+
+        imprimir("Ingrese el tamaño del datagrama: ");
+        tamanioDatagrama = scanner.nextInt();
+
+        imprimir("Ingrese el MTU de la red: ");
+        mtu = scanner.nextInt();
+    }
+
+    public static void imprimir(String texto) {
+        System.out.print(texto);
     }
 }
