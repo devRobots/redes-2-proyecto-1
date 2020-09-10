a902bda9bfd98248d52f989d7aba84b2cb119484
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2020-09-10 13:58:20 -0500

    Documentacion

--- src/co/edu/uniquindio/Main.java
+++ src/co/edu/uniquindio/Main.java
@@ -1,19 +1,46 @@
 package co.edu.uniquindio;
 
-import javax.swing.*;
 import java.util.Scanner;
 
+/**
+ * Proyecto 1 de redes 2
+ * Transferencia de Archivos
+ *
+ * @author Yesid Shair Rosas Toro
+ * @author Juan David Usma Alzate
+ * @author Samara Smith Rincon Montaña
+ */
 public class Main {
 
-    private static String completarDigitos(String bin, int tam) {
-        StringBuilder binBuilder = new StringBuilder(bin);
+    /**
+     * Metodo Completar Digitos
+     * <p>
+     * Completa con 0s los digitos de un numero
+     *
+     * @param num El numero que se va a completar
+     * @param tam El tamaño que deberia tener la cadena de digitos
+     * @return String : El numero num con la cantidad de digitos tam
+     */
+    private static String completarDigitos(String num, int tam) {
+        StringBuilder binBuilder = new StringBuilder(num);
         while (binBuilder.length() < tam) {
             binBuilder.insert(0, "0");
         }
-        bin = binBuilder.toString();
-        return bin;
+        num = binBuilder.toString();
+        return num;
     }
 
+    /**
+     * Metodo Gestionar Fragmentos
+     * <p>
+     * Genera los flags, el offset en binario y decimal y la conversion
+     * hexadecimal de los 16 bits y los retorna en una cadena.
+     *
+     * @param data Longitud del datagrama
+     * @param mtu  Longitud del MTU de la red
+     * @param n    Cantidad de fragmentos resultantes
+     * @return String : La salida procesada
+     */
     private static String gestionarFragmentos(int data, int mtu, int n) {
         StringBuilder salida = new StringBuilder();
         int offset = 0;
@@ -37,28 +64,39 @@ public class Main {
         return salida.toString();
     }
 
+    /**
+     * Metodo main
+     * <p>
+     * Metodo principal donde se ejecuta el codigo
+     */
     public static void main(String[] args) {
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            int tamDatagrama, mtu, cantFragmentos;
-            String resultado = "\n";
+        Scanner scanner = new Scanner(System.in);
+        int tamDatagrama, mtu, cantFragmentos;
+        String resultado = "\n";
 
-            imprimir("Longitud total del datagrama: ");
-            tamDatagrama = scanner.nextInt();
+        imprimir("Longitud total del datagrama: ");
+        tamDatagrama = scanner.nextInt();
 
-            imprimir("Longitud del MTU de la red: ");
-            mtu = scanner.nextInt();
+        imprimir("Longitud del MTU de la red: ");
+        mtu = scanner.nextInt();
 
-            cantFragmentos = tamDatagrama > mtu ? tamDatagrama / mtu + 1 : 1;
-            resultado += "Cantidad de Fragmentos: " + cantFragmentos + "\n\n";
+        cantFragmentos = tamDatagrama > mtu ? tamDatagrama / mtu + 1 : 1;
+        resultado += "Cantidad de Fragmentos: " + cantFragmentos + "\n\n";
 
-            resultado += gestionarFragmentos(tamDatagrama, mtu, cantFragmentos);
+        resultado += gestionarFragmentos(tamDatagrama, mtu, cantFragmentos);
 
-            imprimir(resultado);
-            imprimir("------------------------------------\n\n");
-        }
+        imprimir(resultado);
+        imprimir("------------------------------------\n\n");
     }
 
+    /**
+     * Metodo imprimir
+     * <p>
+     * Imprime un texto
+     * Sirve como alias de System.out.print();
+     *
+     * @param texto Texto a imprimir
+     */
     private static void imprimir(String texto) {
         System.out.print(texto);
     }
