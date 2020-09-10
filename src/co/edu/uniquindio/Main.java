44ae7788f0dae194d9c31d004d05bdc80bfe2bbc
Juan2299 <51072327+Juan2299@users.noreply.github.com>
2020-09-10 01:05:31 -0500

    Update Main.java

--- src/co/edu/uniquindio/Main.java
+++ src/co/edu/uniquindio/Main.java
@@ -19,4 +19,16 @@ public class Main {
     public static void imprimir(String texto) {
         System.out.print(texto);
     }
+    
+        public static int numFragmentos (int data, int mtu)
+    {
+        int dg= data;
+        int num =0;
+        while (dg>0)
+        {
+            dg -= (mtu - 20);
+            num++;
+        }
+        return num;
+    }
 }
