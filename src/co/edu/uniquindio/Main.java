9905aaad35fcf7941675ba3db06a437bf8089fea
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2020-09-17 00:16:29 -0500

    Correcion de Offset

--- src/co/edu/uniquindio/Main.java
+++ src/co/edu/uniquindio/Main.java
@@ -35,7 +35,7 @@ public class Main {
             Fragmento fragmento = new Fragmento(i+1, longitud, flag, offset);
             lista.add(fragmento);
 
-            offset += (mtu - 20);
+            offset += (int) Math.ceil((double)(mtu - 20) / 8);
         }
 
         return lista;
